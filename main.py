6c748bd28894ce7c1d84bd43e76a7b68009e562b
Xinshuang Jin <jinxinshuang@gmail.com>
2025-09-20 18:08:54 -0500

    inorder, preorder, postorder method

--- main.py
+++ main.py
@@ -18,10 +18,61 @@ class Tree:
         self.root = None
 
     def inorder(self):
-        pass
+        values = []
+        return self.inorder_helper(self.root, values)
+
+    def inorder_helper(self, current_node, values):
+        if not current_node:
+            return values
+        
+        # call inorder_helper on left child
+        self.inorder_helper(current_node.left, values)
+
+        # add current_node to list of explored_nodes
+        values.append(current_node)
+
+        # call inorder_helper on right child
+        self.inorder_helper(current_node.right, values)
+
+        return values
 
     def preorder(self):
-        pass
+        # create an empty list to store the explored nodes
+        # call preorder helper on root
+        explored_nodes = []
+        return self.preorder_helper(self.root, explored_nodes)
+    
+    def preorder_helper(self, current_node, explored_nodes):
+        #     if the tree is empty:
+        #         return explored_nodes
+        if not current_node:
+            return explored_nodes
+        
+        # add current_node to list of explored_nodes
+        explored_nodes.append(current_node)
+        # call preorder_helper on left child
+        self.preorder_helper(current_node.left, explored_nodes)
+        # call preorder_helper on right child
+        self.preorder_helper(current_node.right, explored_nodes)
+        # pass
+
+        return explored_nodes
     
     def postorder(self):
-        pass
+        values = []
+        return self.postorder_helper(self.root, values)
+
+    def postorder_helper(self, current_node, values):
+        #     if the tree is empty:
+        #         return explored_nodes
+        if not current_node:
+            return values
+        
+        # call postorder_helper on left child
+        self.postorder_helper(current_node.left, values)
+        # call postorder_helper on right child
+        self.postorder_helper(current_node.right, values)
+        # add current_node to list of explored_nodes
+        values.append(current_node)
+
+        return values
